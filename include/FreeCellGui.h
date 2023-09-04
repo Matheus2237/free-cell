3d9c3df23977b654a7c2dedd190e48f75aaa220c
Matheus Paulino Ribeiro <mathpaulinor@gmail.com>
2023-09-04 01:05:15 -0300

    feat: Implementação da leitura da coluna inicial

--- include/FreeCellGui.h
+++ include/FreeCellGui.h
@@ -1,8 +1,9 @@
 #ifndef FREE_CELL_GUI_H
 #define FREE_CELL_GUI_H
 #define TAMANHO_BARALHO 52
-#define CLEAR_LINE_ABOVE std::cout << "\x1b[1A\x1b[K"
+#define CLEAR_LINE std::cout << "\x1b[1A\x1b[K"
 #include "Carta.h"
+#include "Estrutura.h"
 #include <string>
 
 class FreeCellGui {
@@ -29,15 +30,30 @@ public:
     void imprimeCartas(Carta* cartas) const;
 
     [[deprecated("Método não implementado.")]]
-    unsigned short int leColunaInicial();
+    unsigned short int leColunaInicial(const Estrutura& estrutura);
 
     [[deprecated("Método não implementado.")]]
     unsigned short int leColunaFinal(unsigned short int colunaInicial);
 
 private:
     
-    [[deprecated("Método não implementado.")]]
-    unsigned short int leColuna();
+    /**
+     * @brief Lê do mecanismo de entrada padrão o valor de uma coluna válida,
+     *  ou seja, um valor numérico inteiro que compreende de 0 a 12.
+     * 
+     * @param mensagemLeitura Mensagem a ser exibida para solicitar a entrada de dados.
+     * @return unsigned short int Valor da coluna desejada pelo usuário.
+     */
+    unsigned short int leColuna(const std::string mensagemLeitura);
+
+    /**
+     * @brief Verifica se dada string contém apenas caracteres numéricos.
+     * 
+     * @param colunaString String a ser verificada.
+     * @return true se tiver apenas caracteres numéricos.
+     * @return false se tiver no mínimo um caracter não numérico.
+     */
+    bool somenteNumeros(std::string colunaString);
 };
 
 #endif
--- src/FreeCellGui.cpp
+++ src/FreeCellGui.cpp
@@ -11,8 +11,51 @@ void FreeCellGui::imprimeCartas(Carta* cartas) const {
     std::cout << std::endl << std::endl;
 }
 
-unsigned short int FreeCellGui::leColunaInicial() {}
+unsigned short int FreeCellGui::leColunaInicial(const Estrutura& estrutura) {
+    const std::string mensagemLeitura = "Entre o valor da coluna inicial: ";
+    unsigned short int colunaInicial;
+    bool colunaValida = false;
+    while (!colunaValida) {
+        colunaInicial = this->leColuna(mensagemLeitura);
+        if (colunaInicial == 0) {
+            CLEAR_LINE;
+            std::cout << "Não pode mover carta da saída, tente novamente. ";
+            continue;
+        }
+        colunaValida = true;
+    }
+    return colunaInicial;
+}
 
 unsigned short int FreeCellGui::leColunaFinal(unsigned short int colunaInicial) {}
 
-unsigned short int FreeCellGui::leColuna() {}
+unsigned short int FreeCellGui::leColuna(const std::string mensagemLeitura) {
+    unsigned short int coluna;
+    std::string colunaString;
+    const std::string mensagemErro = "Coluna invalida, tente novamente. ";
+    bool colunaValida = false;
+    while(!colunaValida) {
+        std::cout << mensagemLeitura;
+        std::getline(std::cin, colunaString);
+        if (colunaString.empty() || !this->somenteNumeros(colunaString)) {
+            CLEAR_LINE;
+            std::cout << mensagemErro;
+            continue;
+        }
+        coluna = std::stoi(colunaString);
+        if (coluna < 0 || coluna > 12) {
+            CLEAR_LINE;
+            std::cout << mensagemErro;
+            continue;
+        }
+        colunaValida = true;
+    }
+    return coluna;
+}
+
+bool FreeCellGui::somenteNumeros(std::string colunaString) {
+    for (int i = 0; i < colunaString.length(); i++)
+        if(!isdigit(colunaString[i]))
+            return false;
+    return true;
+}
