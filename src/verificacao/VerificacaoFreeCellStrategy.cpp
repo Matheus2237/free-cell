0b8384808b252e811b6fc218955e9205fde69b35
nicuis <bkscarpa@gmail.com>
2023-09-06 09:00:44 -0300

    feat: Inclusão da verificação das FreeCells.

--- src/verificacao/VerificacaoFreeCellStrategy.cpp
+++ src/verificacao/VerificacaoFreeCellStrategy.cpp
@@ -1,9 +1,8 @@
 #include "../../include/verificacao/VerificacaoFreeCellStrategy.h"
 #include <iostream>
 
-// TODO: Reimplementar regra
 bool VerificacaoFreeCellStrategy::podeMovimentar(unsigned short int colunaInicial,
-        unsigned short int colunaFinal, const Estrutura& estrutura) {
-    std::cout << "Verificacao Free Cell Strategy" << std::endl;
-    return true;
+        unsigned short int colunaFinal, const Estrutura& estrutura){
+
+    return estrutura.encontraUltimaCartaDaColuna(colunaFinal) == COLUNA_VAZIA;
 }
