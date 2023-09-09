1b9c4a98de526d2c3859fe6111b1d17d692a0ee8
Matheus Paulino Ribeiro <mathpaulinor@gmail.com>
2023-09-09 12:20:06 -0300

    test: Inclusão do arquivo de teste de fim de jogo

---
+++ tests/teste_fim_jogo.cpp
@@ -0,0 +1,35 @@
+#include "../include/Baralho.h"
+#include "../include/Estrutura.h"
+#include "../include/FreeCellEngine.h"
+
+void setUp(Estrutura& estrutura) {
+    for (int i = 0; i < TAMANHO_BARALHO; i++) {
+        if (estrutura.getCartas()[i].getValor() != Simbolo::Valor::REI) {
+            estrutura.getCartas()[i].setColuna(0);
+        }
+    }
+    int reisMovidos = 0;
+    while (reisMovidos < 4) {
+        for (int i = 0; i < TAMANHO_BARALHO; i++) {
+            if (estrutura.getCartas()[i].getValor() == Simbolo::Valor::REI) {
+                Carta carta = estrutura.getCartas()[i];
+                for (int j = i; j < TAMANHO_BARALHO-1; j++)
+                    estrutura.getCartas()[j] = estrutura.getCartas()[j+1];
+                estrutura.getCartas()[TAMANHO_BARALHO-1] = carta;
+                break;
+            }
+        
+        }
+        reisMovidos++;
+    }
+}
+
+int main() {
+    Baralho baralho;
+    Estrutura estrutura(baralho.getCartas());
+    setUp(estrutura);
+    FreeCellEngine engine(estrutura);
+    while (!engine.ganhou()) {
+        engine.jogaProximaRodada();
+    }
+}
